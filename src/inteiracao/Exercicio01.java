004c4671a5f04400bb63a4dce58f955da4ca2615
Murilo Henrique <murilo.troxa@hotmail.com>
2022-04-14 01:19:34 -0300

    Initial commit

---
+++ src/inteiracao/Exercicio01.java
@@ -0,0 +1,19 @@
+package inteiracao;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    static final Integer DIVISOR = 3;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer[] numeros = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        for (int i = 0; i < numeros.length; i++) {
+            Integer numero = numeros[i];
+            if (numero % DIVISOR == 0) {
+                System.out.println("O numero "+numero+ " é divisivel por 3 \n");
+            } else {
+                System.out.println("O numero "+numero+ " nao é divisivel por 3 \n");
+            }
+        }
+    }
+}
