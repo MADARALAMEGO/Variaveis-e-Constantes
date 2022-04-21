a8617281f10ce83e0cb6476bcd11d30b0bbec192
Murilo Henrique <murilo.troxa@hotmail.com>
2022-04-21 01:32:34 -0300

    Initial commit

---
+++ src/metodos/Exercicio04.java
@@ -0,0 +1,10 @@
+package metodos;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
