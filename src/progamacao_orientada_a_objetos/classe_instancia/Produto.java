19c5efeff8d5af07fe1335589c454bfed84ea953
Murilo Henrique <murilo.troxa@hotmail.com>
2022-04-30 01:20:16 -0300

    Initial commit

---
+++ src/progamacao_orientada_a_objetos/classe_instancia/Produto.java
@@ -0,0 +1,9 @@
+package progamacao_orientada_a_objetos.classe_instancia;
+
+public class Produto {
+    static Integer quantidadeMinimaEstoque = 1;
+    String nome;
+    String getNome() {
+        return nome;
+    }
+}
