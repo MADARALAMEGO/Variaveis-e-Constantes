0a9bc918d57ecea3c77f713a552b3fc05c53fe06
Murilo Henrique <murilo.troxa@hotmail.com>
2022-05-10 00:14:24 -0300

    Initial commit

---
+++ src/orientacao_a_objetos_pt2/construtores/Pessoa.java
@@ -0,0 +1,11 @@
+package orientacao_a_objetos_pt2.construtores;
+
+public class Pessoa {
+    String nome;
+    int idade;
+
+    public Pessoa (String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
---
+++ src/orientacao_a_objetos_pt2/o_objeto_this/ExemploThis.java
@@ -0,0 +1,14 @@
+package orientacao_a_objetos_pt2.o_objeto_this;
+
+public class ExemploThis {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.modelo = "Palio";
+
+        System.out.println("Modelo antes: " + carro.modelo);
+
+        carro.alterarModelo("Civic", carro);
+
+        System.out.println("Modelo depois: " + carro.modelo);
+    }
+}
