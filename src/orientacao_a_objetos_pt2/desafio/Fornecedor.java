56d88025596136df7ae5ebe457e5188d1f0d0ec8
Murilo Henrique <murilo.troxa@hotmail.com>
2022-05-12 01:41:59 -0300

    Initial commit

---
+++ src/orientacao_a_objetos_pt2/desafio/Fornecedor.java
@@ -0,0 +1,13 @@
+package orientacao_a_objetos_pt2.desafio;
+
+public class Fornecedor {
+    String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
