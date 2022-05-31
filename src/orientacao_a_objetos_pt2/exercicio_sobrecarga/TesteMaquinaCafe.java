3c34f528f4273607f7969b6ca07ee65bdf0df2b2
Murilo Henrique <murilo.troxa@hotmail.com>
2022-05-31 01:52:01 -0300

    Initial commit

---
+++ src/orientacao_a_objetos_pt2/exercicio_sobrecarga/TesteMaquinaCafe.java
@@ -0,0 +1,11 @@
+package orientacao_a_objetos_pt2.exercicio_sobrecarga;
+
+public class TesteMaquinaCafe {
+    public static void main(String[] args) {
+        MaquinaCafe maquinaCafe = new MaquinaCafe();
+        maquinaCafe.acucarDisponivel = 30;
+        maquinaCafe.fazerCafe(10);
+        maquinaCafe.fazerCafe(15);
+        maquinaCafe.fazerCafe();
+    }
+}
